43e934c8130fbb21128ad9a1fc4a5f80cf51ea50
ssaltovsk1 <97896455+ssaltovsk1@users.noreply.github.com>
2022-12-03 09:12:14 +0300

    HomeWork 03 - move questionsList to the variable

--- src/main/java/ru/otus/homeworks/hw02/Main.java
+++ src/main/java/ru/otus/homeworks/hw02/Main.java
@@ -1,10 +1,35 @@
 package ru.otus.homeworks.hw02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+    static ArrayList<Questions> questionsList;
     public static void main(String[] args) {
 
+        questionsList = new ArrayList<>();
+
+        questionsList.add(new Questions(
+                "Как меня зовут?",
+                2,
+                new ArrayList<>(List.of("Никак", "Сергей", "Николай"))
+        ));
+        questionsList.add(new Questions(
+                "Название планеты?",
+                4,
+                new ArrayList<>(List.of("Плутон", "Нептун", "Юпитер", "Венера (правильный ответ)"))
+        ));
+        questionsList.add(new Questions(
+                "Ваш любимый цвет (Белый)?",
+                1,
+                new ArrayList<>(List.of("Белый", "Красный"))
+        ));
+
         System.out.println("Начнем");
+
         PrintOutQuestions.answerQuestions();
     }
+
+
 }
